6173530a68ecbaf1eee3682b38d65ab2623871ed
arthur-mariani <arthurmariani.5@gmail.com>
2025-10-03 19:39:34 -0300

    Main versão 1.0

---
+++ main/rodar/main.java
@@ -0,0 +1,23 @@
+package main.rodar;
+
+    // import main.menus.AlterarPacientes;
+    // import main.menus.CadastrarPacientes;
+    // import main.menus.CadastrarMedicos;
+    // import main.menus.AlterarMedicos;
+
+public class main {
+    public static void main(String[] args) {
+    // CadastrarPacientes cadastroPacientes = new CadastrarPacientes();
+    // cadastroPacientes.cadastrarPaciente();  
+
+    // CadastrarMedicos cadastroMedicos = new CadastrarMedicos();
+    //  cadastroMedicos.cadastrarMedico(); 
+
+    // AlterarPacientes alteraPacientes = new AlterarPacientes();
+    //  alteraPacientes.alterarPaciente();
+
+    // AlterarMedicos alteraMedicos = new AlterarMedicos();
+    //  alteraMedicos.alterarMedico();
+
+    }
+}
